c1a62f38901a5320fe709d47f2ea9ef843640dc6
santiag0ur <83630273+santiag0ur@users.noreply.github.com>
2021-06-05 21:24:28 +0200

    Iteration 1 correction variable, Iteration 2 done

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 // Iteration 1: Names and Input
-const hacker1 = 'Peter';
+let hacker1 = 'Peter';
 let zz = 'The driver´s name is '+ hacker1; 
 console.log (zz); 
-const hacker2 = 'Fredo';
+let hacker2 = 'Fredo';
 let yy = 'The navigator´s name is '+ hacker2; 
 console.log (yy); 
 
 
 // Iteration 2: Conditionals
 
+if ( hacker1.length > hacker2.length) 
+{
+console.log (`The driver has the longest name, it has ${hacker1.length} characters. `);
+}
+else if (hacker2.length > hacker1.length)
+{
+console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+}
+else 
+{
+console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 
 // Iteration 3: Loops
