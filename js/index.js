f9304b11b49524ad728f1d64c3f7e49d929cf71c
santiag0ur <83630273+santiag0ur@users.noreply.github.com>
2021-06-05 16:08:49 +0200

    Iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 // Iteration 1: Names and Input
+const hacker1 = 'Peter';
+let zz = 'The driver´s name is '+ hacker1; 
+console.log (zz); 
+const hacker2 = 'Fredo';
+let yy = 'The navigator´s name is '+ hacker2; 
+console.log (yy); 
 
 
 // Iteration 2: Conditionals
