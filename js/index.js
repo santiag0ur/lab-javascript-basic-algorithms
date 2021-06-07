3113013871d9b26062d85d55f7589245b645b458
santiag0ur <83630273+santiag0ur@users.noreply.github.com>
2021-06-07 23:11:15 +0200

    Iteration 3 done, some simplificationsIteration1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 // Iteration 1: Names and Input
-let hacker1 = 'Peter';
-let zz = 'The driver´s name is '+ hacker1; 
-console.log (zz); 
-let hacker2 = 'Fredo';
-let yy = 'The navigator´s name is '+ hacker2; 
-console.log (yy); 
 
+let hacker1 = 'Alice';
+console.log (`The driver´s name is ${hacker1}.`); 
+let hacker2 = 'Lian';
+console.log (`The navigator´s name is ${hacker2}.`); 
 
 // Iteration 2: Conditionals
 
@@ -23,3 +21,59 @@ console.log (`Wow, you both have equally long names, ${hacker1.length} character
 }
 
 // Iteration 3: Loops
+
+//Iteration 3.1
+let driveruppercased = hacker1.toLocaleUpperCase();
+let deposit = '';
+for (let i = 0; i <driveruppercased.length; i++)
+{
+  deposit = deposit + driveruppercased.slice(i,i + 1) + ' ';
+}
+console.log (deposit);
+
+//Iteration 3.2
+let deposit2 = '';
+for (let i = hacker2.length; i > 0; i--)
+{
+  deposit2 = deposit2 + hacker2.slice(i-1,i);
+}
+console.log (deposit2);
+
+//itearion 3.3
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let navigatoruppercased = hacker2.toLocaleUpperCase();
+let checkword = navigatoruppercased.length - 1;
+let counter = 0;
+while (counter < navigatoruppercased.length)
+  {
+    if (driveruppercased.charAt(counter) !== navigatoruppercased.charAt(counter))
+    {
+      for (let i = 0; i < alphabet.length; i++)
+      {
+        if (driveruppercased.charAt(counter) === alphabet.charAt(i) )
+        {
+        console.log(`The driver's name goes first.`);
+        counter = navigatoruppercased.length;
+        break;
+        }
+        if (navigatoruppercased.charAt(counter) === alphabet.charAt(i) )
+        {
+        console.log(`Yo, the navigator goes first definitely.`);
+        counter = navigatoruppercased.length;
+        break;
+        }
+      }
+    }
+    if (driveruppercased.charAt(counter) === navigatoruppercased.charAt(counter))
+    {
+      if (counter === checkword)
+      {
+        console.log(`What?! You both have the same name?`);
+        break;
+      }
+    }
+  counter++;
+  }
+
+
+
